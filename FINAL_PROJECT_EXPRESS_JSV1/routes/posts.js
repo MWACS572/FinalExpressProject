e65ec6da9401c9f7a9ce3df625287427de81928d
Nolawe <nolawew103@gmail.com>
2017-07-20 20:11:38 -0500

    few changes

--- FINAL_PROJECT_EXPRESS_JSV1/routes/posts.js
+++ FINAL_PROJECT_EXPRESS_JSV1/routes/posts.js
@@ -115,30 +115,69 @@ router.get('/posts/jobOwner/:email', function(req, res, next) {
 //         });
 // });
 
-router.get('/posts/:email', function(req, res, next) {
+// router.get('/posts/:email', function(req, res, next) {
+//
+//     var email=req.params.email;
+//     // console.log(email);
+//     var db = req.db;
+//
+//     var mylocation = {
+//         'location': {
+//             $near: {
+//                 $geometry: { type: "Point", coordinates: [-91.959114, 41.00563]},
+//                 $maxDistance: 10000
+//             }
+//         }
+//
+//     };
+//
+//
+//
+//     var applicantsemail = {applicantsEmail:{$nin: [email]}};
+//     var posteremail = {email:{$ne:email}}
+//
+//     var prefereddate = {"preferedDate": {"$gte":new Date().toISOString()}};
+//
+//     var query={$and:[mylocation,applicantsemail,posteremail, prefereddate]};
+//
+//     var collection = db.bind('post');
+//     collection.find(query).limit(10).toArray(
+//
+//         function(err,data){
+//             if(err)throw err;
+//             /*collection.find({"email": "ermias@yahoo.com"},function(err,data){
+//              if(err)throw err;
+//              */
+//             console.log(data);
+//
+//             res.json(data);
+//         });
+// });
 
-    var email=req.params.email;
-    // console.log(email);
-    var db = req.db;
+router.post('/posts/', function(req, res, next) {
+
+    var  email = req.body.email;
+    var coords = [parseFloat(req.body.long),parseFloat(req.body.lat)];
+
 
     var mylocation = {
         'location': {
             $near: {
-                $geometry: { type: "Point", coordinates: [-91.959114, 41.00563]},
-                $maxDistance: 2000
+                $geometry: { type: "Point", coordinates: coords},
+                $maxDistance: 10000
             }
         }
 
     };
 
-
+    var db = req.db;
 
     var applicantsemail = {applicantsEmail:{$nin: [email]}};
     var posteremail = {email:{$ne:email}}
 
     var prefereddate = {"preferedDate": {"$gte":new Date().toISOString()}};
 
-    var query={$and:[mylocation,applicantsemail,posteremail, prefereddate]};
+    var query={$and:[mylocation,applicantsemail,posteremail,prefereddate]};
 
     var collection = db.bind('post');
     collection.find(query).limit(10).toArray(
@@ -153,6 +192,8 @@ router.get('/posts/:email', function(req, res, next) {
             res.json(data);
         });
 });
+
+
 router.post('/posts/apply/', function(req, res, next) {
     var  email = req.body.email;
     var id =  req.body.id;
